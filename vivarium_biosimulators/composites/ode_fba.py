eec190acf12752f4219a15c263a153c495c78f42
Eran Agmon <agmon.eran@gmail.com>
2021-09-23 15:32:24 -0700

    clean up ODE_FBA Composer and test

--- vivarium_biosimulators/composites/ode_fba.py
+++ vivarium_biosimulators/composites/ode_fba.py
@@ -1,39 +1,30 @@
 
 from biosimulators_utils.sedml.data_model import ModelLanguage
 from vivarium.core.composer import Composer, Composite
+from vivarium.core.engine import pf
 from vivarium.core.composition import simulate_composite
 
 from vivarium_biosimulators.processes.biosimulators_process import BiosimulatorsProcess
 
+SBML_MODEL_PATH = 'vivarium_biosimulators/models/BIOMD0000000297_url.xml'
+BIGG_MODEL_PATH = 'vivarium_biosimulators/models/iAF1260b.xml'
+
+
 class ODE_FBA(Composer):
     defaults = {
-        'ode_simulator': {
-            'biosimulators_api': 'biosimulators_tellurium',
-            'model_source': '',
-            'simulation': 'uniform_time_course',
-            'model_language': ModelLanguage.SBML.value,
-        },
-        'fba_simulator': {
-            'biosimulators_api': 'biosimulators_cobrapy',
-            'model_source': '',
-            'simulation': 'steady_state',
-            'model_language': ModelLanguage.SBML.value,
-        },
-        'flux_mapping': {
-            'ode_rxn_name': 'fba_bounds_name'
-        }
+        'ode_config': {},
+        'fba_config': {},
+        'flux_map': {}  # {'ode_rxn_name': 'fba_bounds_name'}
     }
 
     def generate_processes(self, config):
-        ode_process = BiosimulatorsProcess(config['ode_simulator'])
-        fba_process = BiosimulatorsProcess(config['fba_simulator'])
         return {
-            'ode_process': ode_process,
-            'fba_process': fba_process
+            'ode_process': BiosimulatorsProcess(config['ode_config']),
+            'fba_process': BiosimulatorsProcess(config['fba_config']),
         }
 
     def generate_topology(self, config):
-        # TODO the flux output needs to be connected to fba's bounds input using flux_mapping
+        # TODO the flux output needs to be connected to fba's bounds input using flux_map
         return {
             'ode_process': {
                 'input': ('state',),
@@ -47,23 +38,38 @@ class ODE_FBA(Composer):
 
 
 
-def main():
+def test_ode_fba(
+        total_time=10.,
+):
     config = {
-        'ode_simulator': {
+        'ode_config': {
             'biosimulators_api': 'biosimulators_tellurium',
-            'model_source': ''  # Barton dFBA paper, iFBA paper's ode portion, glc/lcts SBML,
+            'model_source': SBML_MODEL_PATH,
+            'simulation': 'uniform_time_course',
+            'model_language': ModelLanguage.SBML.value,
         },
-        'fba_simulator': {
+        'fba_config': {
             'biosimulators_api': 'biosimulators_cobrapy',
-            'model_source': ''  # load in a BiGG model, e_coli_core, iAF1260b
+            'model_source': BIGG_MODEL_PATH,
+            'simulation': 'steady_state',
+            'model_language': ModelLanguage.SBML.value,
         },
-        'flux_mapping': {}
+        'flux_map': {}
     }
     ode_fba = ODE_FBA(config).generate()
 
-    sim_settings = {}
+    # initial state
+    initial_state = {}
+
+    # run the simulation
+    sim_settings = {
+        'total_time': total_time,
+        'initial_state': initial_state,
+        'display_info': False}
     output = simulate_composite(ode_fba, sim_settings)
+
+    print(pf(output))
 
 
 if __name__ == '__main__':
-    main()
+    test_ode_fba()
